935e4feb76d1e79da17423d76379e1a82ba14914
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-06-13 18:25:13 +0300

    hero component added

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <section>
+      <div class="hero">
+        <div class="hero__img">
+          <img alt="Marvel" src="../assets/marvel.jpg" />
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style></style>

25682b5e060bf410d168ff2e0d42b976bdbe28f8
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-06-13 20:07:56 +0300

    api fix

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -34,14 +34,20 @@ export default {
     };
   },
   created() {
-    const characterId = this?.$route?.params?.id;
+    const characterId = this.$route.params.id;
+
+    const apiUrl = `https://gateway.marvel.com/v1/public/characters/${characterId}`;
+    const apiKey = "60645b73c441bf294a3a3a07b50bfafe";
+    const hash = "2bd018a8f222867e396d936c173bb3d8";
+    const params = {
+      apikey: apiKey,
+      hash: hash,
+    };
 
     axios
-      .get(
-        `https://gateway.marvel.com/v1/public/characters/${characterId}?apikey=60645b73c441bf294a3a3a07b50bfafe&hash=3eec11f4ea14251a0a755a2ff02104b8`
-      )
+      .get(apiUrl, { params })
       .then((response) => {
-        this.character = response?.data?.data?.results[0];
+        this.character = response.data.data.results[0];
       })
       .catch((error) => {
         console.error(error);
@@ -49,3 +55,4 @@ export default {
   },
 };
 </script>
+60645b73c441bf294a3a3a07b50bfafe

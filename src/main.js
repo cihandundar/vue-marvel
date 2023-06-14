2674503fc426fccd85a8a9b70b2a1a311af519b1
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-06-14 12:54:38 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./styles/main.scss";
+import store from "./store";
 createApp(App).use(router).mount("#app");
+
+new Vue({
+  // ...
+  store, // Vuex Store'ı ana Vue instance'ına ekliyoruz
+});
---
+++ src/store.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export default {
+  state: {
+    character: {
+      name: "",
+      thumbnail: {
+        path: "",
+        extension: "",
+      },
+      description: "",
+    },
+  },
+  mutations: {
+    setCharacter(state, character) {
+      state.character = character;
+    },
+  },
+  actions: {
+    fetchCharacter({ commit }, characterId) {
+      const apiUrl = `https://gateway.marvel.com:443/v1/public/characters/${characterId}`;
+      const apiKey = "60645b73c441bf294a3a3a07b50bfafe";
+      const hash = "2bd018a8f222867e396d936c173bb3d8";
+
+      axios
+        .get(apiUrl, {
+          params: {
+            apikey: apiKey,
+            hash: hash,
+          },
+        })
+        .then((response) => {
+          const character = response.data.data.results[0];
+          commit("setCharacter", character);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};

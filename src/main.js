f269ca1af258274f5927af5cde769108f58e6e4c
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-06-14 10:42:58 +0300

    vuex installed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store"; // Vuex store'umuzu import ediyoruz
 import "./styles/main.scss";
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
